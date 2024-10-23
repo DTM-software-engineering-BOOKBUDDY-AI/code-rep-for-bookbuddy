16e1bd355545a64b86762e87797935b3d8965edf
Ali <alireza.shahi@studio.unibo.it>
2024-10-23 12:04:23 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+@app.route('/home')
+def hello_world():
+    return render_template("/home/homepage.html")
+
+@app.route('/Recomendation_page')
+def Recomendation_page():
+    return render_template("/recom page/Recomendation_page.html")
+
+app.run(debug=True)
