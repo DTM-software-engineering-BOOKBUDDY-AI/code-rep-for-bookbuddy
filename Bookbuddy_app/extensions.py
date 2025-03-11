3fe65f04cf8d059f981f35d008e8a9a32112d5da
alireza shahi <alireza.shahi@studio.unibo.it>
2025-03-11 14:05:26 +0100

    fix the problem of form.html not loading without login, with redirecting to the login page insteade

--- Bookbuddy_app/extensions.py
+++ Bookbuddy_app/extensions.py
@@ -1,5 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask import flash, redirect, url_for
 
 db = SQLAlchemy()
 login_manager = LoginManager() 
+login_manager.login_view = 'login'  # Specify the login route
+login_manager.login_message = "Please log in to access this page."
+login_manager.login_message_category = "warning"
+
+# Custom unauthorized handler
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('Please log in to access this page.', 'warning')
+    return redirect(url_for('login')) 
